216b7c7b818149ed3c87e9f430e9ff6bc63767e9
lipeng <1695101826@qq.com>
2020-10-13 23:19:49 +0800

    2020-10-13

---
+++ src/main/java/com/lipeng/javase/multithread/ProdCons_BlockingQueueDemo.java
@@ -0,0 +1,10 @@
+package com.lipeng.javase.multithread;
+
+/**
+ * @program: self-practice
+ * @description:
+ * @author: LiPeng
+ * @create: 2020-10-13 22:49
+ **/
+public class ProdCons_BlockingQueueDemo {
+}
