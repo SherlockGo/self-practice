f20afd777454562bff8e89c878efddd0318c1a80
lipeng <1695101826@qq.com>
2020-10-13 23:23:53 +0800

    prod-cons

--- src/main/java/com/lipeng/algorithm/leetcode/stack/MyQueue.java
+++ src/main/java/com/lipeng/algorithm/leetcode/stack/MyQueue.java
@@ -1,10 +1,57 @@
 package com.lipeng.algorithm.leetcode.stack;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 /**
  * @program: self-practice
- * @description:
+ * @description: 用栈实现队列
  * @author: LiPeng
  * @create: 2020-10-13 22:17
  **/
 public class MyQueue {
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+    private int head;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public int push(int x) {
+        if (s1.isEmpty()){
+            head = x;
+        }
+        while (!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        s2.push(x);
+        while (!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
+        return x;
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        int x = s1.pop();
+        if (!s1.isEmpty()){
+            head = s1.peek();
+        }
+        return x;
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        return head;
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+
 }
--- src/main/java/com/lipeng/javase/multithread/ProdCons_BlockingQueueDemo.java
+++ src/main/java/com/lipeng/javase/multithread/ProdCons_BlockingQueueDemo.java
@@ -1,10 +1,97 @@
 package com.lipeng.javase.multithread;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @program: self-practice
- * @description:
+ * @description: 阻塞队列实现的生产者-消费者模型
  * @author: LiPeng
  * @create: 2020-10-13 22:49
  **/
 public class ProdCons_BlockingQueueDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        MyResource resource = new MyResource(new ArrayBlockingQueue<>(10));
+
+        new Thread(() -> {
+            System.out.println("线程" + Thread.currentThread().getName() + "\t生产线程启动");
+            try {
+                resource.produce();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, "Producer").start();
+
+        new Thread(() -> {
+            System.out.println("线程" + Thread.currentThread().getName() + "\t消费线程启动");
+            System.out.println();
+            try {
+                resource.consume();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, "Consumer").start();
+
+        TimeUnit.SECONDS.sleep(5);
+
+        System.out.println();
+
+        System.out.println("5秒时间到，停止生产和消费");
+
+        resource.stop();
+    }
+
+}
+
+/**
+ * 资源类
+ */
+class MyResource{
+    private volatile boolean flag = true;   //总开关，控制是否生产和消费
+    private AtomicInteger atomicInteger = new AtomicInteger();
+    BlockingQueue<String> queue = null;
+
+    public MyResource(BlockingQueue<String> queue){
+        this.queue = queue;
+    }
+
+    //生产
+    public void produce() throws Exception {
+        String data;
+        boolean retValue;
+        while (flag){
+            data = atomicInteger.incrementAndGet() + "";
+            retValue = queue.offer(data, 2L, TimeUnit.SECONDS);
+            if (retValue){
+                System.out.println("线程" + Thread.currentThread().getName() + "\t插入队列成功" + data);
+            } else {
+                System.out.println("线程" + Thread.currentThread().getName() + "\t插入队列失败" + data);
+            }
+            TimeUnit.SECONDS.sleep(1);
+        }
+        System.out.println("线程" + Thread.currentThread().getName() + "\t停止生产，flag = false.");
+    }
+
+    //消费
+    public void consume() throws Exception {
+        String result;
+        while (flag){
+            result = queue.poll(2L, TimeUnit.SECONDS);
+            if (null == result || result.equals("")){
+                flag = false;
+                System.out.println("线程" + Thread.currentThread().getName() + "\t超过2秒没有获取到元素，退出消费");
+                return;
+            }
+            System.out.println("线程" + Thread.currentThread().getName() + "\t获取元素成功" + result);
+        }
+    }
+
+    //停止
+    public void stop(){
+        flag = false;
+    }
+
 }
